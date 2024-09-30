f9748289393ab72a315f0fe68088046f4942b0cf
Nikita Skovoroda <chalkerx@gmail.com>
2024-09-30 15:06:55 +0400

    drop icap addresses

--- src/get-address.ts
+++ src/get-address.ts
@@ -6,48 +6,6 @@
 
 import * as errors from '@exodus/ethers/errors';
 
-import BN from 'bn.js';
-
-// Shims for environments that are missing some required constants and functions
-var MAX_SAFE_INTEGER: number = 0x1fffffffffffff;
-
-function log10(x: number): number {
-    if (Math.log10) { return Math.log10(x); }
-    return Math.log(x) / Math.LN10;
-}
-
-
-// See: https://en.wikipedia.org/wiki/International_Bank_Account_Number
-
-// Create lookup table
-var ibanLookup: { [character: string]: string } = {};
-for (var i = 0; i < 10; i++) { ibanLookup[String(i)] = String(i); }
-for (var i = 0; i < 26; i++) { ibanLookup[String.fromCharCode(65 + i)] = String(10 + i); }
-
-// How many decimal digits can we process? (for 64-bit float, this is 15)
-var safeDigits = Math.floor(log10(MAX_SAFE_INTEGER));
-
-function ibanChecksum(address: string): string {
-    address = address.toUpperCase();
-    address = address.substring(4) + address.substring(0, 2) + '00';
-
-    var expanded = '';
-    address.split('').forEach(function(c) {
-        expanded += ibanLookup[c];
-    });
-
-    // Javascript can handle integers safely up to 15 (decimal) digits
-    while (expanded.length >= safeDigits){
-        var block = expanded.substring(0, safeDigits);
-        expanded = parseInt(block, 10) % 97 + expanded.substring(block.length);
-    }
-
-    var checksum = String(98 - (parseInt(expanded, 10) % 97));
-    while (checksum.length < 2) { checksum = '0' + checksum; }
-
-    return checksum;
-};
-
 export function getAddress(address, checksum = true): string {
     var result = null;
 
@@ -68,19 +26,8 @@ export function getAddress(address, checksum = true): string {
         if (address.match(/([A-F].*[a-f])|([a-f].*[A-F])/) && result !== address) {
             errors.throwError('bad address checksum', errors.INVALID_ARGUMENT, { arg: 'address', value: address });
         }
-
-    // Maybe ICAP? (we only support direct mode)
     } else if (address.match(/^XE[0-9]{2}[0-9A-Za-z]{30,31}$/)) {
-
-        // It is an ICAP address with a bad checksum
-        if (address.substring(2, 4) !== ibanChecksum(address)) {
-            errors.throwError('bad icap checksum', errors.INVALID_ARGUMENT, { arg: 'address', value: address });
-        }
-
-        result = (new BN(address.substring(4), 36)).toString(16);
-        while (result.length < 40) { result = '0' + result; }
-        result = '0x' + result;
-
+        errors.throwError('ICAP addresses not supported', errors.INVALID_ARGUMENT, { arg: 'address', value: address });
     } else {
         errors.throwError('invalid address', errors.INVALID_ARGUMENT, { arg: 'address', value: address });
     }
