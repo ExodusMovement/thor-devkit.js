bb5dd44b85d4baeb7414a9421d05dba4d5451670
Nikita Skovoroda <chalkerx@gmail.com>
2024-09-30 15:09:55 +0400

    merge into a single file

--- src/abi-coder.ts
+++ src/abi-coder.ts
@@ -2,8 +2,6 @@
 
 /* tslint:disable */
 
-import { getAddress } from './get-address'
-
 export { formatSignature } from '@exodus/ethers/utils/abi-coder'
 import { defaultCoerceFunc } from '@exodus/ethers/utils/abi-coder'
 import { CoerceFunc, ParamType } from '@exodus/ethers/utils/abi-coder' // types
@@ -18,6 +16,37 @@ import { toUtf8Bytes, toUtf8String } from '@exodus/ethers/utils/utf8';
 import { deepCopy, defineReadOnly, shallowCopy } from '@exodus/ethers/utils/properties';
 import { Arrayish } from '@exodus/ethers/utils/bytes';
 import { BigNumberish } from '@exodus/ethers/utils/bignumber';
+
+// Patched getAddress, see https://github.com/vechain/ethers.js/commit/4313595f78c75a60639ad55b86634dd135e217a1
+
+export function getAddress(address, checksum = true): string {
+    var result = null;
+
+    if (checksum) throw new Error('Unexpected')
+
+    if (typeof(address) !== 'string') {
+        errors.throwError('invalid address', errors.INVALID_ARGUMENT, { arg: 'address', value: address });
+    }
+
+    if (address.match(/^(0x)?[0-9a-fA-F]{40}$/)) {
+
+        // Missing the 0x prefix
+        if (address.substring(0, 2) !== '0x') { address = '0x' + address; }
+
+        result = address;
+
+        // It is a checksummed address with a bad checksum
+        if (address.match(/([A-F].*[a-f])|([a-f].*[A-F])/) && result !== address) {
+            errors.throwError('bad address checksum', errors.INVALID_ARGUMENT, { arg: 'address', value: address });
+        }
+    } else if (address.match(/^XE[0-9]{2}[0-9A-Za-z]{30,31}$/)) {
+        errors.throwError('ICAP addresses not supported', errors.INVALID_ARGUMENT, { arg: 'address', value: address });
+    } else {
+        errors.throwError('invalid address', errors.INVALID_ARGUMENT, { arg: 'address', value: address });
+    }
+
+    return result;
+}
 
 const paramTypeBytes = new RegExp(/^bytes([0-9]*)$/);
 const paramTypeNumber = new RegExp(/^(u?int)([0-9]*)$/);
